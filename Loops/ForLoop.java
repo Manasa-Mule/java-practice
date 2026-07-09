ebaf3f16e47a15c84d6db588ad4d60446b6e5593
Manasa-Mule <manasamule317@gmail.com>
2026-07-09 22:18:15 +0530

    Added ForLoop program

---
+++ Loops/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop{
+    public static void main(String[] args) {
+        System.out.println("Numbers from 1 to 10 using For Loop:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
